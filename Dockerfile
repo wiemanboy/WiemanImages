19f66ff5c06904b8a51bd511a8b80a56f897c6b2
jarno <wiemanboy@gmail.com>
2024-07-11 19:53:50 +0200

    ADD: docker build and compose

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,10 @@ WORKDIR /app
 COPY go.mod go.sum ./
 RUN go mod download
 
-COPY . .
+COPY ./main.go ./
+COPY .env ./
+COPY ./config ./config
+COPY ./src ./src
 RUN CGO_ENABLED=0 GOOS=linux go build -o /build
 
 FROM golang:alpine
---
+++ dev/blobstore/create-buckets.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+isAlive() {
+  curl -sf http://127.0.0.1:9000/minio/health/live
+}
+
+# Start Minio in the background
+minio "$@" --quiet & echo $! > /tmp/minio.pid
+
+# Wait for Minio to be ready
+while ! isAlive; do
+  sleep 0.01
+done
+
+# Authenticate
+mc alias set minio http://127.0.0.1:9000 "$MINIO_ROOT_USER" "$MINIO_ROOT_PASSWORD"
+
+# Create buckets
+mc mb minio/test-bucket || true
+mc anonymous set download minio/test-bucket
+
+# Stop Minio
+kill -s INT $(cat /tmp/minio.pid) && rm /tmp/minio.pid
+
+# Wait for Minio to be stopped
+while isAlive; do
+  sleep 0.01
+done
+
+# Start Minio in the foreground
+exec minio "$@"
