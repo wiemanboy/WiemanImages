2ecb85f3d85207e3060e7485bf1b61ef8f42622e
jarno <wiemanboy@gmail.com>
2024-07-30 11:43:57 +0200

    ADD: debug echo's

--- read_secrets.sh
+++ read_secrets.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+echo "Reading secrets from files..."
 for var in $(env); do
   if echo "$var" | grep -q '_FILE='; then
     var_name=$(echo "$var" | cut -d'=' -f1 | sed 's/_FILE$//')
     file_path=$(echo "$var" | cut -d'=' -f2)
     if [ -f "$file_path" ]; then
+      echo "Exporting $var_name from $file_path"
       export "$var_name"="$(cat "$file_path")"
     fi
   fi
