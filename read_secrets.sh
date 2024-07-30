b888a11c9f259f2a6e67aaa3026c6d981d863d8b
jarno <wiemanboy@gmail.com>
2024-07-30 11:55:32 +0200

    ADD: more debugging

--- read_secrets.sh
+++ read_secrets.sh
@@ -2,7 +2,11 @@
 
 echo "Reading secrets from files..."
 for var in $(env); do
+  echo "Reading env variables"
+  echo "$var"
   if echo "$var" | grep -q '_FILE='; then
+    echo "Env variable contains _FILE"
+    echo "$var"
     var_name=$(echo "$var" | cut -d'=' -f1 | sed 's/_FILE$//')
     file_path=$(echo "$var" | cut -d'=' -f2)
     if [ -f "$file_path" ]; then
