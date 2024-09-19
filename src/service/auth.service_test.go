6fd86e23774efb5c6980b881bbd643c6dc15553a
jarno <wiemanboy@gmail.com>
2024-09-19 16:23:38 +0200

    FIX: AuthService tests now run

--- src/service/auth.service_test.go
+++ src/service/auth.service_test.go
@@ -7,24 +7,29 @@ import (
 
 var authService AuthService
 
-func InitAuthServiceTest(m *testing.M) {
+func InitAuthServiceTest() {
 	authService = NewAuthService("secret", 100000, "admin", "admin")
-	m.Run()
 }
 
 func TestAuthService_Login_valid(t *testing.T) {
+	InitAuthServiceTest()
+
 	token, err := authService.Login("admin", "admin")
 	assert.NoError(t, err)
 	assert.NotEmpty(t, token)
 }
 
 func TestAuthService_Login_invalid(t *testing.T) {
+	InitAuthServiceTest()
+
 	token, err := authService.Login("wrongUsername", "wrongPassword")
 	assert.Error(t, err)
 	assert.Empty(t, token)
 }
 
 func TestAuthService_Refresh_valid(t *testing.T) {
+	InitAuthServiceTest()
+
 	validToken, err := authService.Login("admin", "admin")
 	token, err := authService.Refresh(validToken)
 	assert.NoError(t, err)
@@ -32,18 +37,24 @@ func TestAuthService_Refresh_valid(t *testing.T) {
 }
 
 func TestAuthService_Refresh_invalid(t *testing.T) {
+	InitAuthServiceTest()
+
 	token, err := authService.Refresh("invalidToken")
 	assert.Error(t, err)
 	assert.Empty(t, token)
 }
 
 func TestAuthService_Check_valid(t *testing.T) {
+	InitAuthServiceTest()
+
 	validToken, _ := authService.Login("admin", "admin")
 	result := authService.Check(validToken)
 	assert.True(t, result)
 }
 
 func TestAuthService_Check_invalid(t *testing.T) {
+	InitAuthServiceTest()
+
 	result := authService.Check("invalidToken")
 	assert.False(t, result)
 }
